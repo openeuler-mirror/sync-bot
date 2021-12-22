18d2d0945fbd3b06608f637858df2b866d4855b7
liheavy <lihaiwei8@huawei.com>
2021-12-22 10:03:52 +0800

    放开openeuler中docs仓库限制

--- hook/pull_request.go
+++ hook/pull_request.go
@@ -422,7 +422,7 @@ func (s *Server) HandlePullRequestEvent(e gitee.PullRequestEvent) {
 
 	// TODO: need to be configurable
 	// ignoring repo in openeuler
-	if owner == "openeuler" {
+	if owner == "openeuler" && repo != "docs" {
 		logger.Infoln("Ignoring repo in openeuler")
 		return
 	}
